102612e04e020dc620f5f1fbe18476643b480e74
Nicolas MILON <nicolas.milon@flowdesk.co>
2024-07-13 14:35:16 +0200

    add wallet creation

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
+
 from dotenv import load_dotenv
 import gpt_module
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext, MessageHandler, filters
 import json
 from dotenv import load_dotenv
-import gpt_module
 import os
+import uuid
+from circle.web3 import utils, developer_controlled_wallets
+import base64
+from ens.auto import ns
 # Start command handler
 
+global gpt_client
+global circle_client
+global circle_wallet_set_id
+
+def lookup_wallet_uuid(tg_user_id: str):
+    print(f"getting wallet uuid for {tg_user_id}")
+    return '2fd61b7b-b24b-541c-b373-55a90eaa008d'
 
 async def start(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text('Hello! I am TxGPT Bot, your friendly AI assistant designed to make blockchain transactions easy and accessible!')
+    global circle_wallet_set_id
+    wallet_uuid = lookup_wallet_uuid(update.message.from_user.id)
+    # if wallet_uuid is not None:
+    #     await update.message.reply_text('Hello! I am TxGPT Bot, your account is already ready to use !')
+    #     return
+    
+    wallet_client = developer_controlled_wallets.WalletsApi(circle_client)
+    create_wallet_response = wallet_client.create_wallet(
+        create_wallet_request=developer_controlled_wallets.CreateWalletRequest.from_dict(
+            {        
+                "accountType": 'SCA',
+                "blockchains": ['MATIC-AMOY'],
+                "walletSetId": circle_wallet_set_id,
+            }
+        )
+    )
+    print(create_wallet_response)
+    await update.message.reply_text('Hello! I am TxGPT Bot, your friendly AI assistant designed to make blockchain transactions easy and accessible! Your account is already ready to use !') 
 
 # Help command handler
 
@@ -22,8 +50,7 @@ async def help_command(update: Update, context: CallbackContext) -> None:
 
 async def handle_natural_language_message(update: Update, context: CallbackContext) -> None:
     user_message = update.message.text
-    client = gpt_module.GptClient()
-    response = client.call_with_prompt(user_message)
+    response = gpt_client.call_with_prompt(user_message)
     print(response.choices[0].message.content)
     # Preprocess the user message with LLM and return the response in a JSON format
     await update.message.reply_text(f'You said: {user_message}')
@@ -35,6 +62,12 @@ async def handle_natural_language_message(update: Update, context: CallbackConte
 def main():
     load_dotenv()
     # Create the Application and pass it your bot's token
+    global gpt_client
+    global circle_client
+    global circle_wallet_set_id
+    circle_wallet_set_id = os.environ["CIRCLE_WALLET_SET_ID"]
+    gpt_client = gpt_module.GptClient()
+    circle_client = utils.init_developer_controlled_wallets_client()
     
     application = Application.builder().token(os.environ['TXGPT_TOKEN']).build()
 
